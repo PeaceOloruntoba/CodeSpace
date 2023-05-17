a7865185c7162159cb808318cb20a141614ee9fa
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-17 02:23:51 +0000

    sdfgh

---
+++ main.py
@@ -0,0 +1,5 @@
+print(" Peace Oloruntoba ")
+print(" This repository is going to contain some of my python projects, and I would love to hear your review about them")
+print(" if they are bad or goog? ")
+mail = "peaceoloruntoba22@gmail.com"
+print(" You can send in your reviews to my email: " + mail + ". I look forward to hearing from you all")
